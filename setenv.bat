e4aeaaf3b4fbb49bf4849a4697cf2713ed0fa2ef
aizwellenstan <aizwellenstan@gmail.com>
2023-05-16 16:11:25 +0800

    Update setenv.bat

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,4 @@
-set VS_VERSION = "2022"
-set CMAKE_PATH = "C:\Program Files\Microsoft Visual Studio\%VS_VERSION%\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
+set VS_VERSION=2022
+set CMAKE_PATH=C:\Program Files\Microsoft Visual Studio\%VS_VERSION%\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin
 
 setx PATH "%CMAKE_PATH%;%PATH%" /M
